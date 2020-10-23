1319b9967454797083dfe0334653929dd681f65b
Sara Wilcox <saraewilcox@gmail.com>
2020-10-23 15:14:04 +0100

    all done

--- js/index.js
+++ js/index.js
@@ -1,104 +1,72 @@
-// Set the variables
-let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
-let frames = 0;
-let requestId = 0;
-let _car;
-let _background;
-
-// Start game
-function startGame() {
-  obstacles = [];
-  _car = new Car(canvas.width);
-  requestId = 1;
-  update();
-}
-
-window.onload = () => {
-  window.addEventListener("keydown", event => {
-    if (event.key === "ArrowRight") {
-      moveRight();
-    }
-    if (event.key === "ArrowLeft") {
-      moveLeft();
-    }
-  });
-
-  document.getElementById("start-button").onclick = () => {
+let currentGame;
+let currentCar;
+document.getElementById('game-board').style.display = 'none';
+const myCanvas = document.getElementById('the-canvas');
+const ctx = myCanvas.getContext('2d');
+document.getElementById('start-button').onclick = () => {
     startGame();
-  };
-
-  _background = new Background(canvas.width, canvas.height);
-};
-
-// Background
-class Background {
-  constructor(canvas_width, canvas_height) {
-    this.x = 0;
-    this.y = 0;
-    this.width = canvas_width;
-    this.height = canvas_height;
-    this.image = new Image();
-    this.image.src = "./images/road.png";
-  }
-
-  draw() {
-    context.drawImage(this.image, this.x, this.y, this.width, this.height);
-    context.drawImage(
-      this.image,
-      this.x,
-      this.y - this.height,
-      this.width,
-      this.height
-    );
-    this.y += 4;
-    if (this.y >= this.height) this.y = 0;
-  }
+}
+document.onkeydown = (e) => {
+    let whereToGo = e.keyCode;
+    currentGame.car.moveCar(whereToGo);
+}
+function startGame() {
+    document.getElementById('game-board').style.display = 'block';
+    //Instantiate a new game of the game class
+    currentGame = new Game();
+    //Instantiate a new car
+    currentCar = new Car();
+    currentGame.car = currentCar;
+    currentCar.drawCar();
+    updateCanvas();
 }
 
-// Car
-class Car {
-  constructor(canvas_width) {
-    this.x = 250;
-    this.y = 520;
-    this.width = 70;
-    this.height = 170;
-    this.canvas_width = canvas_width;
-    this.image = new Image();
-    this.image.src = "./images/car.png";
-  }
-  //Move the Car
-  draw() {
-    context.drawImage(this.image, this.x, this.y, this.width, this.height);
-  }
+function detectCollision(obstacle) {
+    return !((currentCar.y > obstacle.y + obstacle.height) ||
+    (currentCar.x + currentCar.width < obstacle.x) ||
+    (currentCar.x - currentCar.width > obstacle.x + obstacle.width))
+}
 
-  moveRight() {
-    if (this.x <= this.canvas_width - (this.width + 50)) {
-      this.x += 50;
+let obstaclesFrequency = 0;
+function updateCanvas() {
+    ctx.clearRect(0, 0, 500, 600); //width and height of the canvas
+    currentGame.car.drawCar();
+    obstaclesFrequency++;
+    if (obstaclesFrequency % 100 === 1) { //more obstacles wanted, make number less than 100!
+        //draw obstacle
+        let randomObstacleX = Math.floor(Math.random() * 450);
+        let randomObstacleY = 0; //it will always appear starting at the top
+        let randomObstacleWidth = Math.floor(Math.random() * 50) + 20;
+        let randomObstacleHeight = Math.floor(Math.random() * 50) + 20;
+        let newObstacle = new Obstacle(randomObstacleX, randomObstacleY, randomObstacleWidth, randomObstacleHeight);
+
+    currentGame.obstacles.push(newObstacle);
     }
-  }
+    //console.log(currentGame.obstacles); //test to see if you can create new objects into the array. refresh page, console tab shows the array being added to.
 
-  moveLeft() {
-    if (this.x >= 100) {
-      this.x -= 50;
+    for(let i=0; i<currentGame.obstacles.length; i++) {
+        currentGame.obstacles[i].y += 1;
+        currentGame.obstacles[i].drawObstacle();
+        console.log(currentGame.obstacles);
+        if (detectCollision(currentGame.obstacles[i])) {
+            alert('YOU DIE!!');
+            obstaclesFrequency = 0; //now you need to end the game so no endless loop of alerts
+            currentGame.score = 0;
+            document.getElementById('score').innerHTML = 0;
+            currentGame.obstacles = [];
+            document.getElementById('game-board').style.display='none';
+        }
+        
+
+        if (currentGame.obstacles.length > 0 && currentGame.obstacles[i].y >= 600) { 
+            currentGame.obstacles.splice(i, 1);
+            currentGame.score++;
+            document.getElementById('score').innerHTML = currentGame.score;
+        }
     }
-  }
-}
 
-function moveLeft() {
-  if (_car) _car.moveLeft();
-}
-
-function moveRight() {
-  if (_car) _car.moveRight();
-}
-//Move the background and car
-function update() {
-  frames++;
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
-  _background.draw();
-  _car.draw();
-  requestId = requestAnimationFrame(update); 
-
+    requestAnimationFrame(updateCanvas);
+   /* setInterval(() => {
+        updateCanvas();
+    }, 60);*/
 }
